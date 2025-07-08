35c62cf084e8af616a92ca1ad44b95d2bbc20ff3
cccc <3372544840@qq.com>
2025-07-08 20:29:29 +0800

    删除重复文件夹

--- domain/user.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type User struct {
-	Id       int64
-	Email    string
-	Password string
-}
--- repository/dao/init.go
+++
@@ -1,7 +0,0 @@
-package dao
-
-import "gorm.io/gorm"
-
-func Inittable(db *gorm.DB) error {
-	return db.AutoMigrate(&User{})
-}
